e9cc3726235d17465af61ecf114c1021fb082fd1
lambert snx <lambertsnx@outlook.com>
2022-01-25 10:21:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RapportModule;
+
+Console.WriteLine("RESULTATEN");
+Resultaat mijnpunten = new Resultaat();
+var lijstResultaten = new List<int>() {40, 50, 5, 90, 100,0};
+foreach(int behaaldeScore in lijstResultaten) 
+{
+    mijnpunten.Percentage = behaaldeScore;
+    mijnpunten.PrintGraad();
+}
---
+++ Resultaat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapportModule
+{
+    internal class Resultaat
+    {
+        public int Percentage { get; set; } = -1;
+
+        internal void PrintGraad()
+        {
+            if (Percentage < 0)
+            {
+                throw new Exception("Percentage niet toegewezen");
+            }
+            string message;
+            if (Percentage > 85)
+            {
+                message = "Grootste Onderscheiding";
+            }
+            else if (Percentage > 75)
+            {
+                message = "Grote Onderscheiding";
+            }
+            else if (Percentage > 68)
+            {
+                message = "Onderscheiding";
+            }
+            else if (Percentage > 50)
+            {
+                message = "voldoende";
+            }
+            else
+            {
+                message = "Niet Geslaagd";
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
+
+
+
+/*
+
+< 50: niet geslaagd;
+tussen 50 en 68: voldoende;
+tussen 68 en 75: onderscheiding;
+tussen 75 en 85: grote onderscheiding;
+> 85: grootste onderscheiding.
+*/
